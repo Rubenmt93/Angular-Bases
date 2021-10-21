d739668ea4d3c9d3b20cf93bb2fb0c92bb518cb0
Ruben Montes <rubenmontes330@gmail.com>
2021-10-21 11:45:57 +0100

    Heroe Component 2

--- src/app/heroes/heroe/heroe.component.ts
+++ src/app/heroes/heroe/heroe.component.ts
@@ -6,5 +6,21 @@ import { Component } from "@angular/core";
     templateUrl: 'heroe.component.html'
 })
 export class HeroeComponent{
+    nombre: string = 'Ironman';
+    edad  : number = 45;
 
+    get nombreCapitalizado():string{
+        return this.nombre.toUpperCase();
+    }
+    
+    obtenerNombre(): string{
+        return `${this.nombre} -${this.edad}`;
+    }
+
+    cambiarNombre():void{
+        this.nombre='SpiderMan';
+    }
+    cambiarEdad():void{ 
+        this.edad=18;
+    }
 }
